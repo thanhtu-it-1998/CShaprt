9ea87bd9cbbc5deb2d6b4b228122aa87b3e03b07
It-Truongthanhtu <truongthanhtu.it.1998@gmail.com>
2020-10-16 08:32:45 +0700

    Update: Unit6 universal windowns

---
+++ UWP/UI6/App1/App1/Model/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Model
+{
+    public class Book
+    {
+        int intBookID;
+        string strTitle, strAuthor, strCoverImage;
+
+        public int IntBookID { get => intBookID; set => intBookID = value; }
+        public string StrTitle { get => strTitle; set => strTitle = value; }
+        public string StrAuthor { get => strAuthor; set => strAuthor = value; }
+        public string StrCoverImage { get => strCoverImage; set => strCoverImage = value; }
+
+
+    }
+    public class BookManager
+    {
+        public static List<Book> GetBooks()
+        {
+            var objBooks = new List<Book>();
+
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+            objBooks.Add(new Book { IntBookID = 1, StrTitle = "Vulpate", StrAuthor = "Futurum", StrCoverImage = "Assets/StoreLogo.png" });
+
+            return objBooks;
+        }
+    }
+}
