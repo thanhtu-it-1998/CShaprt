4dc426a2194e4f81ef9807887967f896c27c0fd6
It-Truongthanhtu <truongthanhtu.it.1998@gmail.com>
2020-10-27 09:21:12 +0700

    UDPATE'

---
+++ PractiveUniversalWindowns/PractiveUniversalWindowns/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using PractiveUniversalWindowns.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PractiveUniversalWindowns
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<ImageProduct> ImageProducts;
+        private ObservableCollection<Product> Products;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ImageProducts = new List<ImageProduct>();
+
+            ImageProducts.Add(new ImageProduct { ImageProductPath = "/Assets/ImageProduct/Product1.png" });
+            ImageProducts.Add(new ImageProduct { ImageProductPath = "/Assets/ImageProduct/Product2.png" });
+            ImageProducts.Add(new ImageProduct { ImageProductPath = "/Assets/ImageProduct/Product3.png" });
+            ImageProducts.Add(new ImageProduct { ImageProductPath = "/Assets/ImageProduct/Product4.png" });
+            ImageProducts.Add(new ImageProduct { ImageProductPath = "/Assets/ImageProduct/Product5.png" });
+            ImageProducts.Add(new ImageProduct { ImageProductPath = "/Assets/ImageProduct/Product6.png" });
+
+            Products = new ObservableCollection<Product>();
+            Products.Add(new Product { NameProduct = "Iphone 1", DescriptionProduct = "Truong Thanh Tu da mua", ImagePath = "/Assets/ImageProduct/Product1.png" });
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            string image = ((ImageProduct)ImageComboBox.SelectedValue).ImageProductPath;
+            Products.Add(new Product{ NameProduct = NameProduct.Text, DescriptionProduct = DescriptionProduct.Text, ImagePath = image});
+            NameProduct.Text = "";
+            DescriptionProduct.Text = "";
+            ImageComboBox.SelectedIndex = -1;
+            NameProduct.Focus(FocusState.Programmatic);
+        }
+    }
+}
---
+++ PractiveUniversalWindowns/PractiveUniversalWindowns/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiveUniversalWindowns.Model
+{
+   public class Product
+    {
+        public string NameProduct { get; set; }
+        public string DescriptionProduct { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
