0ca652e303de882b12e2e59783665797dc9213b3
It-Truongthanhtu <truongthanhtu.it.1998@gmail.com>
2020-11-02 21:15:04 +0700

    Udpate

---
+++ Net/ASP/WingtipToys/Site.Master.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WingtipToys
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public IQueryable<Category> GetCategories()
+        {
+            var db = new WingtipToysEntities();
+            IQueryable<Category> query = db.Categories;
+            return query;
+        } 
+    }
+}
