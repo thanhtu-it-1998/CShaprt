54e48c31db9fe12e486adf0db1777ee307d92c1f
It-Truongthanhtu <truongthanhtu.it.1998@gmail.com>
2020-11-03 11:37:29 +0700

    update

---
+++ Net/Lab01ASP/Site.Master.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Lab01ASP.Models;
+using Microsoft.AspNet.Identity;
+
+namespace Lab01ASP
+{
+    public partial class SiteMaster : MasterPage
+    {
+        private const string AntiXsrfTokenKey = "__AntiXsrfToken";
+         private const string AntiXsrfUserNameKey = "__AntiXsrfUserName";
+        private string _antiXsrfTokenValue;
+
+        protected void Page_Init(object sender,EventArgs e)
+        {
+            var requestCookie = Request.Cookies[AntiXsrfTokenKey];
+            Guid requestCookieGuiValue;
+            if(requestCookie != null && Guid.TryParse(requestCookie.Value,out requestCookieGuiValue))
+            {
+                _antiXsrfTokenValue = requestCookie.Value;
+                Page.ViewStateUserKey = _antiXsrfTokenValue;
+            }
+            else
+            {
+                _antiXsrfTokenValue = Guid.NewGuid().ToString("N");
+                Page.ViewStateUserKey = _antiXsrfTokenValue;
+
+                var responseCookie = new HttpCookie(AntiXsrfTokenKey)
+                {
+                    HttpOnly = true,
+                    Value = _antiXsrfTokenValue
+                };
+                if(FormsAuthentication.RequireSSL&& Request.IsSecureConnection)
+                {
+                    requestCookie.Secure = true;
+                }
+                Response.Cookies.Set(responseCookie);
+            }
+            Page.PreLoad += master_Page_PreLoad;
+        }
+
+        protected void master_Page_PreLoad(object sender,EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ViewState[AntiXsrfTokenKey] = Page.ViewStateUserKey;
+                ViewState[AntiXsrfUserNameKey] = Context.User.Identity.Name ?? String.Empty;
+
+            }
+            else
+            {
+                if((String)ViewState[AntiXsrfTokenKey] != _antiXsrfTokenValue || (string)ViewState[AntiXsrfUserNameKey] != (Context.User.Identity.Name?? String.Empty))
+                {
+                    throw new InvalidOperationException("Validation of Anti - XSRF token failed.");
+                }
+            }
+        }
+
+        protected void Page_PreRender(Object sender,EventArgs e)
+        {
+            using (ShoppingCartActions userShoppingCart = new ShoppingCartActions())
+            {
+                string cartStr=string.Format($"Cart ({userShoppingCart.GetCount()})");
+                cartCount.InnerText = cartStr;
+            }
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
+        //{
+        //    Context.GetOwinContext().Authentication.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+        //}
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new Lab01ASP.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
+        }
+
+        protected void Unnamed_LoggingOut(object sender,LoginCancelEventArgs e)
+        {
+            Context.GetOwinContext().Authentication.SignOut();
+        }
+    }
+
+}
