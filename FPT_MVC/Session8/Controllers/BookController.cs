1bdce54eb4d445079cc6d5c9927d22c00be1781c
It-Truongthanhtu <truongthanhtu.it.1998@gmail.com>
2020-11-09 11:21:20 +0700

    UPDATE

---
+++ FPT_MVC/Session8/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Session8.Models;
+
+namespace Session8.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Get: Book/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post: Book/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("ID,Titile,RealaseDate,Cost")] BookModel bookModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(bookModel);
+        }
+    }
+}
