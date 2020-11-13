0515761c312e0e97934b40440abe82a5d913a9a2
It-Truongthanhtu <truongthanhtu.it.1998@gmail.com>
2020-11-13 23:08:51 +0700

    update

---
+++ FPT_MVC/RoleBaseAppAccess/RoleBaseAppAccess/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using RoleBaseAppAccess.CustomFiler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoleBaseAppAccess.Controllers
+{
+    public class AdminController:Controller
+    {
+        [AuthLog(Roles ="Administrator")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FPT_MVC/RoleBaseAppAccess/RoleBaseAppAccess/Controllers/RoleController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using RoleBaseAppAccess.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace RoleBaseAppAccess.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        /// <summary>  
+        /// Get All Roles  
+        /// </summary>  
+        /// <returns></returns>  
+        public ActionResult Index()
+        {
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+        /// <summary>  
+        /// Create  a New role  
+        /// </summary>  
+        /// <returns></returns>  
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        /// <summary>  
+        /// Create a New Role  
+        /// </summary>  
+        /// <param name="Role"></param>  
+        /// <returns></returns>  
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
